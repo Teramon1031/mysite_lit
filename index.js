1edca5f1ccc73d8a769882832f1d861175f18832
Teramon1031 <teramon.jump02@gmail.com>
2023-05-04 20:11:52 +0900

    2023-05-04-20-12

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+// アコーディオンスタイル
+// $(function () {
+//   $(".what").on("click", function () {
+//     var $thisDetail = $(".detail").eq($(".what").index($(this)));
+//     if ($thisDetail.hasClass("active")) {
+//       //   $thisDetail.fadeOut("slow");
+//       $thisDetail.removeClass("active");
+//     } else {
+//       //   $thisDetail.fadeIn("slow");
+//       $(".detail").removeClass("active");
+//       $(".what").removeClass("figureOn");
+//       $(this).addClass("figureOn");
+//       $thisDetail.addClass("active");
+//     }
+//   });
+// });
+
 $(function () {
-  $("figure").on("click", function () {
-    var $thisDetail = $(".detail").eq($("figure").index($(this)));
-    if ($thisDetail.hasClass("active")) {
-      //   $thisDetail.fadeOut("slow");
-      $thisDetail.removeClass("active");
-    } else {
-      //   $thisDetail.fadeIn("slow");
-      $(".detail").removeClass("active");
-      $("figure").removeClass("figureOn");
-      $(this).addClass("figureOn");
-      $thisDetail.addClass("active");
-    }
+  // スクロールを開始したら
+  $(window).on("scroll", function () {
+    // ファーストビューの高さを取得
+    $(".what").each(function () {
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight - 100) {
+        $(this).addClass("figureOn");
+      }
+      if (scroll > position - windowHeight + windowHeight / 4) {
+        $(this).removeClass("figureOn");
+      }
+      // if (scroll > position + windowHeight / 4) {
+      //   $(this).addClass("figureOn");
+      // }
+    });
   });
 });
